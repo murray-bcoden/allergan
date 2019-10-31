42ef3abdfc043f23ada1f40e5fe2854df4cbbf12
Joe McCorison <joemccorison@murraygroup.ie>
2019-10-31 16:54:35 +0000

    image optimizations

--- optimize.js
+++ optimize.js
@@ -3,7 +3,7 @@ const imageminJpegtran = require('imagemin-jpegtran');
 const imageminPngquant = require('imagemin-pngquant');
 
 (async () => {
-    const files = await imagemin(['src/assets/img/inline/*.{jpg,png}','src/assets/img/bg/*.{jpg,png}'], {
+    const files = await imagemin(['assets/images/*.{jpg,png}'], {
         destination: 'src/assets/img/inline/',
         plugins: [
             imageminJpegtran({
